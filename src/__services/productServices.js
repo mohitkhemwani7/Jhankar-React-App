afcb1a15b1e8b3e9cff61038988df8ef7659845b
mohitkk7 <mohit.khemwani@talentica.com>
2024-09-19 14:27:19 +0530

    Test Commit

--- src/__services/productServices.js
+++ src/__services/productServices.js
@@ -1,33 +1,25 @@
-import {authHeader} from "../__helpers/authHeader";
-import {myConfig as config} from '../config';
+import {myConfig as config} from '../config'
 
-export const productServices = {
+export const ProductPageServices = {
     getProducts,
 };
-
-function getProducts() {
-    const requestOptions = {
-        method: 'GET',
-        headers: authHeader()
-    };
-
-    return fetch(`${config.apiUrl}/listproducts/`, requestOptions)
-        .then(handleResponse)
-        .then(
-            data => {
-                return data;
-            }
-        )
-}
 
 function handleResponse(response) {
     return response.text().then(text => {
         const data = text && JSON.parse(text);
         if (response.status === 200) {
             return data;
-        } else {
-            const error = (data && data.message) || response.statusText;
-            return Promise.reject(error);
+        } else if (response.status === 500) {
+            console.log("Error");
         }
     })
+}
+
+function getProducts(category, kitchenName) {
+    const requestOptions = {
+        method: 'GET',
+    };
+
+    return fetch(`${config.apiUrl}/api/products`)
+        .then(handleResponse)
 }
